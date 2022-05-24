97413fb5b5d2e7543e18e24ff87caea87d31a44d
BrattiestClub00 <ampengilly@hotmail.com>
2022-05-24 21:50:28 +0800

    Check String for Numbers

--- ISAD1000_Project.java
+++ ISAD1000_Project.java
@@ -13,6 +13,13 @@ import java.util.Scanner;
 public class ISAD1000_Project {
     public static void main(String[] args)
     {
+        startNumCheck();
+        startStringCon();
+    }
+
+    //This method runs the stringConversion methods allowing for user input
+    public static void startStringCon()
+    {
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Enter string you would like to convert: ");
         String inString = keyboard.nextLine();
@@ -41,6 +48,49 @@ public class ISAD1000_Project {
         }
 
         return outString;
+    }
+
+    //This method runs the checkForNumbers methods allowing for user input
+    public static void startNumCheck()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("\nEnter String you would like to check for numbers: ");
+        String inString = keyboard.nextLine();
+        checkForNumbers(inString);
+    }
+
+    //This method checks whether numeric values are in a string provided by the user
+    public static void checkForNumbers(String inString)
+    {
+        boolean containsNumbers = false;
+        String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+
+        String[] numbersFound = new String[inString.length()];
+
+        for(int i = 0; i < numbers.length; i++)
+        {
+            if(inString.contains(numbers[i]))
+            {
+                containsNumbers = true;
+                numbersFound[i] = numbers[i];
+            }
+        }
+
+        if(containsNumbers == true)
+        {
+            System.out.println("Numbers were found: \n");
+            for(int i = 0; i < numbersFound.length; i++)
+            {
+                if(numbersFound[i] != null)
+                {
+                    System.out.printf("%s ,", numbersFound[i]);
+                }
+            }
+        }
+        else
+        {
+            System.out.println("NO numbers were found");
+        }
     }
 
 
