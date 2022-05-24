fb61c0e3063f56133d2ea75e0da86bd62c6ce83e
BrattiestClub00 <ampengilly@hotmail.com>
2022-05-24 22:22:34 +0800

    remove nums from String

--- ISAD1000_Project.java
+++ ISAD1000_Project.java
@@ -11,12 +11,13 @@ import java.util.Scanner;
 public class ISAD1000_Project {
     public static void main(String[] args)
     {
-        startStringValid();
+        
+        //startStringValid();
         //startNumCheck();
         //startStringCon();
     }
 
-    //This method runs the stringConversion methods allowing for user input
+    //This method runs the stringConversion method allowing for user input
     public static void startStringCon()
     {
         Scanner keyboard = new Scanner(System.in);
@@ -50,50 +51,41 @@ public class ISAD1000_Project {
         return outString;
     }
 
-    //This method runs the checkForNumbers methods allowing for user input
+    //This method runs the checkForNumbers method allowing for user input
     public static void startNumCheck()
     {
         Scanner keyboard = new Scanner(System.in);
         System.out.print("\nEnter String you would like to check for numbers: ");
         String inString = keyboard.nextLine();
-        checkForNumbers(inString);
+        if(checkForNumbers(inString) == true)
+        {
+            System.out.println("String contains numbers");
+        }
+        else
+        {
+            System.out.println("String does not contain numbers");
+        }
         keyboard.close();
     }
 
-    //This method checks whether numeric values are in a string provided by the user
-    public static void checkForNumbers(String inString)
+    //This method checks whether numeric values are in a string provided by the user returns boolean value
+    public static boolean checkForNumbers(String inString)
     {
         boolean containsNumbers = false;
         String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-
-        String[] numbersFound = new String[inString.length()];
 
         for(int i = 0; i < numbers.length; i++)
         {
             if(inString.contains(numbers[i]))
             {
                 containsNumbers = true;
-                numbersFound[i] = numbers[i];
             }
         }
 
-        if(containsNumbers == true)
-        {
-            System.out.println("Numbers were found: \n");
-            for(int i = 0; i < numbersFound.length; i++)
-            {
-                if(numbersFound[i] != null)
-                {
-                    System.out.printf("%s ,", numbersFound[i]);
-                }
-            }
-        }
-        else
-        {
-            System.out.println("NO numbers were found");
-        }
+        return containsNumbers;
     }
 
+    //This method runs the stringValid method allowing for user input
     public static void startStringValid()
     {
         Scanner keyboard = new Scanner(System.in);
@@ -125,6 +117,46 @@ public class ISAD1000_Project {
 
         return valid;
     }
+
+    //This method removes numbers from a string and returns the string, this method is used for compoundStringOp method
+    public static String removeNumbersFromString(String inString)
+    {
+        String outString = "";
+
+        char[] array = inString.toCharArray();
+        
+        for(int i = 0; i < array.length; i++)
+        {
+            if(Character.isDigit(array[i]) == false)
+            {
+                outString = outString + array[i];
+            }
+        }
+
+        return outString;
+    }
+
+
+    //This method is compound String operations: It removes any numerical values in a user provided string
+            //It then converts the string to upper or lower case, again the users choice
+    public static String compoundStringOp(String inString)
+    {
+        String outString = "";
+        
+        if(checkForNumbers(inString) == true)
+        {
+            outString = removeNumbersFromString(inString);
+        }
+        else
+        {
+            outString = inString;
+        }
+        
+
+
+        return outString;
+    }
+
 
     
 }
